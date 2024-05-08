6a228ee6e104a8d279964fba4a63d3b3be127010
Anna Powaska <annapowaska@op.pl>
2024-05-08 11:59:04 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Czas from './czas.js';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const App = () => {
+  const [time, setTime]= useState(0);
+  const [timer, setTimer] = useState();
+
+  const startStoper = () =>{
+    setTimer(setInterval(() =>
+    { setTime(prevValue => prevValue + 10);}))
+  };
+
+  const stopStoper = () =>{
+    clearInterval(timer);}
+
+  const resetStoper = () => {
+    stopStoper();
+    setTime(0);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Czas time={time}/>
+      <button onClick={startStoper}>START</button>
+      <button onClick={stopStoper}>STOP</button>
+      <button onClick={resetStoper}>RESET</button>
     </div>
   );
-}
+  }
 
 export default App;
---
+++ src/czas.js
@@ -0,0 +1,15 @@
+
+
+const Czas =  props => {
+
+    const timer = () => { 
+       new Date()
+    }
+    return (
+      <div>
+        {timer}
+      </div>
+    );
+ 
+}
+export default Czas
