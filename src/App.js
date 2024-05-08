c6a69ff6b05a152d903f000c918d3f42bd8fb130
Anna Powaska <annapowaska@op.pl>
2024-05-08 11:59:17 +0200

    New

--- src/App.js
+++ src/App.js
@@ -5,28 +5,12 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 const App = () => {
-  const [time, setTime]= useState(0);
-  const [timer, setTimer] = useState();
-
-  const startStoper = () =>{
-    setTimer(setInterval(() =>
-    { setTime(prevValue => prevValue + 10);}))
-  };
-
-  const stopStoper = () =>{
-    clearInterval(timer);}
-
-  const resetStoper = () => {
-    stopStoper();
-    setTime(0);
-    }
+ 
 
   return (
     <div>
-      <Czas time={time}/>
-      <button onClick={startStoper}>START</button>
-      <button onClick={stopStoper}>STOP</button>
-      <button onClick={resetStoper}>RESET</button>
+      <Czas />
+   
     </div>
   );
   }
